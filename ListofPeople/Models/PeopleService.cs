52b7e0b05352829256ba11a9ec6975dabb6c9268
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-04-26 10:27:42 +0200

    04-26

--- ListofPeople/Models/PeopleService.cs
+++ ListofPeople/Models/PeopleService.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ListofPeople.Models
+namespace ListofPeople.Models//new
 {
     public class PeopleService : IPeopleservice
     {
